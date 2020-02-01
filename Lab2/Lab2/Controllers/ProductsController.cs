7664c2f7866ea342981fc6c0ea4554e675d8699a
SyedAbbas1214 <s01061178@acad.tri-c.edu>
2020-02-01 02:02:08 -0500

    Lab 2 Initial Commit, added Products Controller.

---
+++ Lab2/Lab2/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            ViewBag.Message = " Product/Index is displayed ";
+            return View();
+        }
+        public string Browse()
+        {
+            return " Browse displayed ";
+        }
+        public string Details(int id)
+        {
+            string message = " Details displayed for Id = " + id;
+            return message;
+        }
+    }
+}
